f3f9f33aa11a96efaf25ac4c8eb0726dbb83efe0
Kei-Bright <kei@bright-development.com>
2022-06-17 15:01:04 -0400

    added axios

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const axios = require('axios');
+
 
 
 function HomeScreen() {
   const navigation = useNavigation();
+  const goToDetails = () => navigation.navigate('Details');
+
+  async function hitAPI() {
+    const result = await axios.get('https://api.magicthegathering.io/v1/sets');
+    setCardSets(result.data.sets.length)
+  }
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log('counter: ', counter)
+  }, [counter])
+
+  function IncrementCounter() {
+    setCounter(counter + 1);
+  }
+
+  const [cardSets, setCardSets] = useState(0);
+  console.log(cardSets);
+  useEffect(() => {
+    hitAPI()
+  }, [])
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
-        <Text>Home Screen</Text>
+      <TouchableOpacity onPress={IncrementCounter}>
+        <Text>{cardSets}</Text>
       </TouchableOpacity>
 
     </View>
