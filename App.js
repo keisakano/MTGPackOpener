2b0e94fb6307fdbd7fc85301dc8b85e67a718f6f
Kei-Bright <kei@bright-development.com>
2022-06-20 14:18:21 -0400

    put mtg set data into flatlist component

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import setButton from './src/components/setButton';
 const axios = require('axios');
 
 
 
 function HomeScreen() {
+
   const navigation = useNavigation();
   const goToDetails = () => navigation.navigate('Details');
 
-  async function hitAPI() {
+  const hitAPI = async function () {
     const result = await axios.get('https://api.magicthegathering.io/v1/sets');
     const setsData = result.data.sets;
-    setCardSets(setsData.length)
+    // setCardSets(setsData.length)
+    setCardSets(setsData)
     for (let cardSets of setsData) {
       console.log(cardSets)
     }
@@ -22,22 +25,30 @@ function HomeScreen() {
 
   const [cardSets, setCardSets] = useState(0);
 
-  console.log(cardSets);
-
-
   useEffect(() => {
     hitAPI()
   }, [])
 
+
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TouchableOpacity>
-        <Text>{cardSets}</Text>
-      </TouchableOpacity>
+      <FlatList
+        data={cardSets}
+        renderItem={({ item }) => (
+          // ^^not quite sure how renderItem works, ask Luke
+          <TouchableOpacity>
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        )}
 
+      />
     </View>
   );
 }
+
+
+
 
 function Details() {
   return (
