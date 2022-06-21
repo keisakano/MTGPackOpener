2d5b77bce289f69aff82404a1cb0fcf2718aa032
Kei-Bright <kei@bright-development.com>
2022-06-21 11:58:04 -0400

    initialized conole.log of booster data

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import DetailsList from './DetailsList';
+
 const axios = require('axios');
 
 
@@ -11,11 +13,13 @@ const ResultsList = () => {
     const hitAPI = async function () {
         const result = await axios.get('https://api.magicthegathering.io/v1/sets');
         const setsData = result.data.sets;
+        const cardData = await axios.get('https://api.magicthegathering.io/v1/sets/ktk/booster')
+        console.log(cardData.data.cards);
         // setCardSets(setsData.length)
         setCardSets(setsData)
-        for (let cardSets of setsData) {
-            console.log(cardSets)
-        }
+        // for (let cardSets of setsData) {
+        //     console.log(cardSets)
+        // }
     }
 
     const [cardSets, setCardSets] = useState(0);
@@ -24,10 +28,20 @@ const ResultsList = () => {
         hitAPI()
     }, [])
 
+    const detailView = () => {
+        <View>
+            <FlatList
+
+            />
+        </View>
+    }
+
     const renderItem = ({ item }) => (
+        // <TouchableOpacity onPress={() => goToDetails()}>
         <TouchableOpacity onPress={() => goToDetails()}>
             <Text>{item.name}</Text>
         </TouchableOpacity>
+        // <DetailsList title={item.name} onPress={() => goToDetails()} />
     )
     return (
         <FlatList
