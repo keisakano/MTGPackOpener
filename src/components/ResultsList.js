5a1ac6b5fdb3b92529b012279f6c4a685e7a0332
Kei-Bright <kei@bright-development.com>
2022-07-06 18:55:12 -0400

    semi broken filter resultList, but renders filtered sets

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, TextInput } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import SearchBar from './SearchBar';
 
 const axios = require('axios');
 
@@ -19,22 +18,22 @@ const ResultsList = () => {
         const sR = setsData.filter(set => set.set_type === 'expansion' || set.set_type === 'core');
         console.log(sR);
         setCardSets(setsData);
-        console.log(setsData)
         setSortedSets(sR);
         // console.log(result)
     }
 
     const [cardSets, setCardSets] = useState([]);
 
+    const [searchWord, setSearchWord] = useState('');
+
+    const [searchField, setSearchField] = useState('');
+
+    const [sortedSets, setSortedSets] = useState([]);
 
 
     useEffect(() => {
         hitAPI()
     }, [])
-
-    const [sortedSets, setSortedSets] = useState([]);
-
-
 
     const renderItem = ({ item }) => {
         const { name: setName, code: setCode } = item;
@@ -47,10 +46,43 @@ const ResultsList = () => {
         )
     }
 
+    const handleChange = e => {
+        e.preventDefault();
+        setSearchField(e.target.value);
+    };
+    const names = sortedSets;
+
+    const filteredSets = names.filter(set => {
+        console.log(set.name)
+        return (
+            set.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+    })
+    console.log(filteredSets)
+
+    useEffect(() => {
+        console.log('prop', names)
+    }, [])
+
+
+
+
+    // const filteredSets = names.filter(set => {
+    //     console.log(set.name)
+    //     return (
+    //         set.name.toLowerCase().includes(searchField.toLowerCase())
+    //     )
+    // })
+
     const header = () => {
         return (
-            <View>
-                <SearchBar />
+            <View style={styles.search}>
+                <TextInput
+                    placeholder="Search Sets"
+                    id="searchInput"
+                    onChange={handleChange}
+                // ^^figure out how to remove default styling when selected
+                ></TextInput>
             </View>
         );
     };
@@ -59,10 +91,11 @@ const ResultsList = () => {
         <FlatList
             showsVerticalScrollIndicator={false}
             ListHeaderComponent={header}
-            data={sortedSets}
+            data={filteredSets}
             renderItem={renderItem}
             keyExtractor={item => item.code}
         />
+
     )
 
 }
@@ -76,6 +109,15 @@ const styles = StyleSheet.create({
     },
     touchables: {
         marginVertical: 5
+    },
+    search: {
+        borderWidth: 2,
+        borderColor: 'black',
+        paddingVertical: 5,
+        paddingHorizontal: 3,
+        marginVertical: 10,
+        borderRadius: 5,
+        fontSize: 15
     }
 })
 
